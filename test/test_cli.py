a24e051bc043111641ef149a3318fa2e6246e316
Stefan Hagen <stefan@hagen.link>
2022-11-01 19:03:49 +0100

    Extende fixtures and tests

--- test/test_cli.py
+++ test/test_cli.py
@@ -4,6 +4,7 @@ import tekstialue.cli as cli
 
 TEST_PREFIX = pathlib.Path('test', 'fixtures', 'basic')
 DEFAULT_IN_FILE_PATH = TEST_PREFIX / 'in-file.tex'
+DEFAULT_CFG_PATH = TEST_PREFIX / 'dot.tekstialue.json'
 TEST_MAKE_MISSING = 'missing-this-file-for-'
 
 
@@ -13,4 +14,36 @@ def test_parse_request(capsys):
     assert options.in_file == str(DEFAULT_IN_FILE_PATH)  # type: ignore
     out, err = capsys.readouterr()
     assert not out
+    assert not err
+
+
+def test_parse_request_verbose(capsys):
+    options = cli.parse_request(['-i', str(DEFAULT_IN_FILE_PATH), '-v'])
+    assert options.verbose
+    out, err = capsys.readouterr()
+    assert not out
+    assert not err
+
+
+def test_parse_request_config(capsys):
+    options = cli.parse_request(['-i', str(DEFAULT_IN_FILE_PATH), '-c', str(DEFAULT_CFG_PATH)])
+    assert options.cfg_file == str(DEFAULT_CFG_PATH)
+    out, err = capsys.readouterr()
+    assert not out
+    assert not err
+
+
+def test_parse_request_config_home(capsys):
+    options = cli.parse_request(['-i', str(DEFAULT_IN_FILE_PATH)])
+    assert options.cfg_file == ''
+    out, err = capsys.readouterr()
+    assert not out
+    assert not err
+
+
+def test_parse_empty_request(capsys):
+    options = cli.parse_request([])
+    assert options == 0
+    out, err = capsys.readouterr()
+    assert 'input' in out
     assert not err
---
+++ test/test_tekstialue.py
@@ -0,0 +1,43 @@
+import pathlib
+
+import tekstialue.tekstialue as api
+
+TEST_PREFIX = pathlib.Path('test', 'fixtures', 'basic')
+DEFAULT_CFG_PATH = TEST_PREFIX / 'dot.tekstialue.json'
+DEFAULT_REAL_IN_FILE_PATH = TEST_PREFIX / 'real-la.tex.in'
+
+
+def test_cue_tables():
+    assert api.cue_tables([]) == []
+
+
+def test_cue_tables_for_real():
+    with open(DEFAULT_REAL_IN_FILE_PATH, 'rt', encoding=api.ENCODING) as handle:
+        lines = [''] + [line.rstrip() for line in handle.readlines()]
+    assert api.cue_tables(lines) == [
+        {
+            'start': 2,
+            'end_data_row': [21, 24],
+            'top_rule': 7,
+            'mid_rule': 17,
+            'end_head': 18,
+            'bottom_rule': 25,
+            'end': 26,
+            'amend': 28,
+        }
+    ]
+
+
+def test_discover_configuration():
+    code, cfg, cp = api.discover_configuration(DEFAULT_CFG_PATH)
+    assert not code
+    assert cp == str(DEFAULT_CFG_PATH)
+    cols = cfg['columns']
+    assert cols['col_1'] == ['a']
+    assert cols['col_2'] == ['b']
+    assert cols['col_3'] == ['c', 'cc']
+    assert cols['col_4'] == ['d', 'dd']
+
+
+def test_extract_slots():
+    assert api.extract_slots([{'start': 0, 'amend': 42}]) == [(0, 42 + 1)]
